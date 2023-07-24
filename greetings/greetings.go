96628b8b092966d1f4359493aedee497435983b0
Devonta <hello@devinhrrs.me>
2023-07-24 00:25:31 -0400

    adding more work

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,11 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 	// Declare a variable == var message string
 	//var message string = fmt.Sprintf("Hi, %v. Welcome!", name)
 
+	// If no name was given, return an error with a message.
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"rsc.io/quote"
+	"example/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+
+	// Set properties of the predefined Loggger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+
+	message, err := greetings.Hello("Devin")
+
+	// If an error was returned, print it to the console and exit the program.
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error print to console
+
+	fmt.Println(message)
 }
